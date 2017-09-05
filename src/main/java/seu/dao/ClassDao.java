f95f59d739df549e9e2b97c10ccbe37a15ae6265
shellqiqi <shellqiqi@outlook.com>
2017-09-05 16:16:22 +0800

    Add dao

---
+++ src/main/java/seu/dao/ClassDao.java
@@ -0,0 +1,16 @@
+package seu.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClassDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+}
