ba7d3504df4558163c6af3f40a79ff38b161ffb1
wtharry <1795024144@qq.com>
2017-09-05 21:15:25 +0800

    write domain.course

--- src/main/java/seu/domain/Course.java
+++ src/main/java/seu/domain/Course.java
@@ -4,4 +4,49 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Course {
+    private int courseID;
+    private String courseName;
+    private double credit;
+    private int period;
+    private int teacherID;
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(int period) {
+        this.period = period;
+    }
+
+    public int getTeacherID() {
+        return teacherID;
+    }
+
+    public void setTeacherID(int teacherID) {
+        this.teacherID = teacherID;
+    }
 }
