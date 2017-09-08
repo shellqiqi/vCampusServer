5fbaecaa9475b78207548c1f6aacd429e7caf77a
NaNa <ynayb@foxmail.com>
2017-09-08 23:20:06 +0800

    update service

--- src/main/java/seu/service/DormitoryService.java
+++ src/main/java/seu/service/DormitoryService.java
@@ -3,6 +3,8 @@ package seu.service;
 import seu.dao.DormitoryDao;
 import seu.domain.Dormitory;
 
+import java.util.List;
+
 public class DormitoryService {
     DormitoryDao dormitoryuser;
 
@@ -14,7 +16,28 @@ public class DormitoryService {
         this.dormitoryuser = dormitoryuser;
     }
 
-    public int addDormitory(final Dormitory dormitory) {
+    public int insertDormitory(final Dormitory dormitory) {
         return dormitoryuser.insertDormitory(dormitory);
     }
+
+    public int insertDormitory(final int DormitoryID, final int Score) {
+        return dormitoryuser.insertDormitory(DormitoryID, Score);
+    }
+
+    public int deleteDormitoryByID(final int DormitoryID) {
+        return dormitoryuser.deleteDormitoryByID(DormitoryID);
+    }
+
+    public int updateScoreByID(final int DormitoryID, final int Score) {
+        return dormitoryuser.updateScoreByID(DormitoryID, Score);
+    }
+
+    public int queryScoreByDormitoryID(final int DormitoryID) {
+        return dormitoryuser.queryScoreByDormitoryID(DormitoryID);
+    }
+
+    //public List<Dormitory> queryDormitoryByStudentId(final int studentId) {
+    //    return dormitoryuser.queryDormitoryByStudentId(studentId);
+  //  }
+
 }
