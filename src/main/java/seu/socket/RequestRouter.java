51aa72aa29b7b01159dbacbf231ce1defd25a3a8
shell7 <shellqiqi@outlook.com>
2017-09-14 10:20:22 +0800

    delete todo of request router

--- src/main/java/seu/socket/RequestRouter.java
+++ src/main/java/seu/socket/RequestRouter.java
@@ -9,7 +9,7 @@ import java.lang.reflect.Method;
 
 @Component
 public class RequestRouter {
-    //TODO: 测试
+
     private ApplicationContext context;
 
     @Autowired
@@ -18,30 +18,8 @@ public class RequestRouter {
     }
 
     public ServerResponse getResponse(ClientRequest clientRequest) {
-//        String requestType = clientRequest.getRequestType();
-
-//        switch (requestType) {
-//            case "getAll":
-//                return nonParam(clientRequest.getServiceName(), clientRequest.getMethodName());
-//            case "getOne":
-//            case "delete":
-////                return intParam(clientRequest.getServiceName(), clientRequest.getMethodName(), Integer.class, clientRequest.getParam());
-//            case "update":
-//            case "add":
-//                return objParam(clientRequest.getServiceName(), clientRequest.getMethodName(), Object.class, clientRequest.getParam());
-//        }
         return new ServerResponse(invokeMethod(clientRequest.getServiceName(), clientRequest.getMethodName(), clientRequest.getParamTypes(), clientRequest.getParams()));
     }
-
-//    public Object nonParam(String serviceName, String methodName) {
-//        Method method = ReflectionUtils.findMethod(context.getBean(serviceName).getClass(), methodName);
-//        return ReflectionUtils.invokeMethod(method, context.getBean(serviceName));
-//    }
-//
-//    public Object objParam(String serviceName, String methodName, Class aClass, Object param) {
-//        Method method = ReflectionUtils.findMethod(context.getBean(serviceName).getClass(), methodName, aClass);
-//        return ReflectionUtils.invokeMethod(method, context.getBean(serviceName), aClass, param);
-//    }
 
     private Object invokeMethod(String beanName, String methodName, Class<?>[] paramTypes, Object[] params) {
         Method method;
@@ -53,9 +31,4 @@ public class RequestRouter {
             return ReflectionUtils.invokeMethod(method, context.getBean(beanName), params);
         }
     }
-
-//    public Object intParam(String serviceName, String methodName, Class aClass, Object param) {
-//        Method method = ReflectionUtils.findMethod(context.getBean(serviceName).getClass(), methodName, aClass);
-//        return ReflectionUtils.invokeMethod(method, context.getBean(serviceName), aClass, param);
-//    }
 }
